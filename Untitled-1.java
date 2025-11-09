ecab2f8a6027b8cd8339431bfb8c685fd15284bf
n8tee <overduekhanooo@gmail.com>
2025-11-09 13:39:13 -0600

    Project pseudo code

---
+++ Untitled-1.java
@@ -0,0 +1,46 @@
+/* Open the fileName */
+File file=new File( fileName ); Scanner
+input; try { input = new Scanner(file);
+}catch(Exception e)
+{
+e.printStackTrace();
+System.out.println("The file "+fileName+" was not found."); return;
+}
+
+Department name object
+Initialize Getters and setters
+
+Queue pq = new PriorityQueue<>();
+
+itemsDesired (requests, price) {
+    add to department list
+
+}
+
+//Distributing budge algorithm
+while(budget > 0) {
+    remove and save the front department of the PQ
+
+    while (items == true & price of iten > budget) {
+        move item to itemsRemoved list
+    }
+    if (no items) {
+        scholarship = $1000
+    }
+    else {
+        next item is purchased
+    }
+    update PriorityQueue
+    add item to itemsReceived list
+    add dept. back to PQ
+    subtract the amount from budget
+    print purchased item
+}
+
+
+//Output
+print in order of purchased
+list depts. items received and not received
+list the total amount of the budget spent + percentage of total budget
+
+
